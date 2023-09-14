b7abdf0e7d34313759349a11b6a4507da0076972
Arhik <arhik23@gmail.com>
2023-09-14 20:28:28 +0530

    including windows platform window code
    This is old code which worked on windows. Just including this to support cross platform applications.

--- src/WGPUCore.jl
+++ src/WGPUCore.jl
@@ -1904,7 +1904,7 @@ elseif Sys.isapple()
 elseif Sys.islinux()
     include("glfw.jl")
 elseif Sys.iswindows()
-    include("glfw.jl") # TODO windows is not tested yet
+    include("glfwWindows.jl") # TODO windows is not tested yet
 end
 
 function destroy(texView::GPUTextureView)
