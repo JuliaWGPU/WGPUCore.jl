55e472dd5239bc590d84fb4a76dff5fcf24280e9
Arhik <arhik23@gmail.com>
2023-09-14 21:03:25 +0530

    `wgpuCreateInstance` is called multiple times
    `wgpuInstance` is now a reference and needs to dererenced to check for null pointer.

--- src/instance.jl
+++ src/instance.jl
@@ -2,7 +2,7 @@ wgpuInstance = Ref{WGPUInstance}()
 
 function getWGPUInstance()
     global wgpuInstance
-    if wgpuInstance != C_NULL
+    if wgpuInstance[] == C_NULL
         instanceDesc = cStruct(WGPUInstanceDescriptor)
         instanceDesc.nextInChain = C_NULL
         wgpuInstance[] = wgpuCreateInstance(instanceDesc  |> ptr)
