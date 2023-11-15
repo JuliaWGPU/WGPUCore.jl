6a16939e4115293f75049b6982b940804e5a190b
Documenter.jl <documenter@juliadocs.github.io>
2023-11-15 07:09:53 +0000

    build based on ebc21d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WGPUCore","category":"page"},{"location":"#WGPUCore","page":"Home","title":"WGPUCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WGPUCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WGPUCore]","category":"page"}]
+}
